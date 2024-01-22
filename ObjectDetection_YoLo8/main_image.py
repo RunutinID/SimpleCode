d8e37817bd0efd5dd41d96f1fee47667138a3700
Nuur-R <firdausnuurr@gmail.com>
2024-01-22 20:40:18 +0700

    Object Detection Yolo8

--- ObjectDetection_YoLo8/main_image.py
+++ ObjectDetection_YoLo8/main_image.py
@@ -1,15 +1,10 @@
 from PIL import Image
 from ultralytics import YOLO
 
-# Load a pretrained YOLOv8n model
 model = YOLO('models/yolov8n.pt')
-
-# Run inference on 'bus.jpg'
-results = model('test/London.png')  # results list
-
-# Show the results
+results = model('test/London.png')
 for r in results:
-    im_array = r.plot()  # plot a BGR numpy array of predictions
-    im = Image.fromarray(im_array[..., ::-1])  # RGB PIL image
-    im.show()  # show image
-    im.save('results/London-Results.png')  # save image
+    im_array = r.plot()
+    im = Image.fromarray(im_array[..., ::-1])
+    im.show()
+    im.save('results/London-Results.png')
